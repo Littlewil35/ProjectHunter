8cdb5afa61e21531284fb528e9b4c81e0e3a3618
wak7222 <wak7222@rit.edu>
2019-09-18 06:04:21 -0400

    i think database is kind of working now?

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,28 @@ $(function(){
     console.log("ready!!!")
 })
 
-$("#newProjoButton").click(function(){
-   alert("aaaa");  
-})
+
+
+$(function() {
+      $("#button").click( function()
+           {
+                $.ajax({
+                url: '/signUpUser',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+           });
+             console.log(document.getElementById('testInput').value);
+           }
+      );
+
+
+});
+
+
 
