2d9a5ebd0f6dca8aee45b187f106950a5158a3bb
wak7222 <wak7222@rit.edu>
2019-12-02 17:04:25 -0500

    Nav bar works! next step is doing some re-naming and actually implementing enums

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from importlib.resources import contents
-
-from flask import Flask, render_template, request, jsonify
-from sqlalchemy import Column, Integer, String
-from flask_sqlalchemy import SQLAlchemy
 from enum import Enum
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+
 
 class status(Enum):
     done = 1
     in_prog = 2
     avail = 3
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -20,7 +20,6 @@ class Post(db.Model):
     tags = Column(String)
     post = Column(String)
     status = Column(String)
-
 
     def __repr__(self):
         return "<Post(name='%s', tags='%s', post='%s', status='%s')>" % (self.name, self.tags, self.post, self.status)
@@ -38,15 +37,15 @@ def to_arr(arr):
         data.append((Post.name, Post.tags, Post.post))
     return data
 
+
 @app.route("/")
 def hello():
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/william/ProjectHunter.sqlite'
     db.create_all()
     data = to_arr(Post.query.all())
     m_posts = to_arr(Post.query.filter_by(status='current').all())
-    print(data)
+    data.__repr__()
     return render_template("index.html", dbData=data)
-
 
 
 @app.route("/process", methods=['POST'])
@@ -55,10 +54,25 @@ def test():
     tag = request.form['tags']
     body = request.form['body']
     status = request.form['status']
-    test_post = Post(name,tag,body,status)
+    test_post = Post(name, tag, body, status)
     db.session.add(test_post)
     db.session.commit()
     return jsonify('output', str)
+
+@app.route("/upforgrabs")
+def query_inprog():
+    m_posts = to_arr(Post.query.filter_by(status='avail').all())
+    return render_template("index.html", dbData=m_posts)
+
+@app.route("/seekingcolab")
+def query_done():
+    m_posts = to_arr(Post.query.filter_by(status='in_prog').all())
+    return render_template("index.html", dbData=m_posts)
+
+@app.route("/completed")
+def query_avail():
+    m_posts = to_arr(Post.query.filter_by(status='done').all())
+    return render_template("index.html", dbData=m_posts)
 
 
 
